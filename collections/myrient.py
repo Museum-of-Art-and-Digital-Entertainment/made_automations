98c959e97036d26243b20466eac3f4cd8f131590
David Chidester <davidchidester@protonmail.com>
2026-03-23 14:21:36 -0700

    fix error with forward slash

--- collections/myrient.py
+++ collections/myrient.py
@@ -34,6 +34,7 @@ def download(game: Game):
     fname = parse.unquote(game.title)
     logger.info(f"downloading {fname}")
     try:
+        logger.debug(game.link)
         res = requests.get(game.link)
         if res.status_code != 200:
             logger.error(f"{fname} download failed, http status {res.status_code}")
@@ -47,7 +48,7 @@ def download(game: Game):
 if __name__ == "__main__":
     libs = [
             "Microsoft%20-%20Xbox%20360%20(Digital)",
-            "Nintendo%20-%20Nintendo%20DSi%20%28Digital%29/",
+            "Nintendo%20-%20Nintendo%20DSi%20%28Digital%29",
             "Nintendo%20-%20Wii%20%28Digital%29%20%28CDN%29",
             "Nintendo%20-%20Wii%20U%20%28Digital%29%20%28CDN%29",
             "Sony%20-%20PlayStation%203%20%28PSN%29%20%28Content%29",
@@ -62,7 +63,7 @@ if __name__ == "__main__":
     assert choice in range(len(libs)), "invalid choice"
     downloadPath = input("Enter download path (leave black for current directory): ") or "."
     domain = "https://myrient.erista.me"
-    library = f"{domain}/files/No-Intro/{libs[choice]}"
+    library = f"{domain}/files/No-Intro/{libs[choice]}/"
     links = get_game_list(library)
     gameList = []
     for game in links:
